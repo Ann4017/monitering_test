f780491a724bf5a219e9b53d7fdd05a9fde5930f
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-03-07 02:59:25 +0900

    Update main.go

--- get_text/main.go
+++ get_text/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"get_text/db"
+	"log"
 )
 
 func main() {
@@ -12,4 +14,11 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(info)
+
+	dataSource := info.s_user + ":" + info.s_pwd + "@tcp(" + info.s_host + ":" + info.s_port + ")/" + info.s_database
+	db, err := sql.Open(info.s_engine, dataSource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
