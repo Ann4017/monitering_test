6efff9adbccb857b76acffbf99f35d15c7caef0e
Ann4017 <121239306+Ann4017@users.noreply.github.com>
2023-03-09 10:24:01 +0900

    commit

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func main() {
+	info := &DB_info{}
+
+	err := info.Get_ini()
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	dataSource := info.s_user + ":" + info.s_pwd + "@tcp(" + info.s_host + ":" + info.s_port + ")/" + info.s_database
+	db, err := sql.Open(info.s_engine, dataSource)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	fmt.Println(dataSource)
+}
